601c775e5f7ad0e34096b104ac252daadc967716
Isaac Mosquera <imosquer@gmail.com>
2017-01-16 22:21:49 -0800

    blah

--- setup.py
+++ setup.py
@@ -17,6 +17,6 @@ setup(name='hello-deploy',
      ],
      data_files=[
         ('/etc/init/',['armory/scripts/armory-hello-server.conf']),
-        ('armory/hellodeploy/',['static/*', 'templates/*'])
+        ('armory',['hellodeploy/static/*', 'hellodeploy/templates/*'])
     ]
 )
