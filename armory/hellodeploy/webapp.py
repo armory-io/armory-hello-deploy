49ba32bb7640607b078f67ebdf0b220114379828
Isaac Mosquera <imosquer@gmail.com>
2017-01-16 21:58:01 -0800

    changing name for func

--- armory/hellodeploy/webapp.py
+++ armory/hellodeploy/webapp.py
@@ -5,7 +5,7 @@ from armory.hellodeploy import kv_parser
 server = Flask(__name__, static_url_path='/static')
 
 @server.route("/")
-def healthcheck():
+def home():
     kv_text = open('/etc/default/server-env').read()
     env_kv = kv_parser.parse(kv_text)
     return render_template('index.html', userdata=env_kv)
