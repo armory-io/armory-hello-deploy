826fa5ced6f5dcedce2605db2266afc3977079ff
Isaac Mosquera <imosquer@gmail.com>
2017-01-18 12:07:03 -0800

    pushing common config files

--- Jenkinsfile
+++ Jenkinsfile
@@ -4,9 +4,12 @@ node {
     stage("Build Image") {
         sh("arm build")
     }
-    
+
     stage("Build Image") {
-        sh("arm push")
+        sh '''
+          source /mnt/secrets/bintray
+          arm push
+        '''
     }
 
 }
--- setup.py
+++ setup.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python
-
+import os
 from distutils.core import setup
 
+build_number = os.environ.get("BUILD_NUMBER", "0")
 setup(name='hello-deploy',
-    version='1.0',
+    version='0.%s' % build_number,
     description='Armory Hello Deploy Site',
     author='Isaac Mosquera',
     author_email='isaac@armory.io',
